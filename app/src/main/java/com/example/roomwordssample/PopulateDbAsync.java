3f5fe69f377b3106de2953166af47de1c0a66b79
ilyasch199 <ilyasch199@gmail.com>
2022-05-16 21:49:49 +0100

    lab

---
+++ app/src/main/java/com/example/roomwordssample/PopulateDbAsync.java
@@ -0,0 +1,30 @@
+package com.example.roomwordssample;
+
+import android.os.AsyncTask;
+
+import com.example.roomwordssample.dao.WordDao;
+import com.example.roomwordssample.entities.Word;
+
+class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+    private final WordDao mDao;
+    String[] words = {"dolphin", "crocodile", "cobra"};
+
+    PopulateDbAsync(WordRoomDatabase db) {
+        mDao = db.wordDao();
+    }
+
+    @Override
+    protected Void doInBackground(final Void... params) {
+        // Start the app with a clean database every time.
+        // Not needed if you only populate the database
+        // when it is first created
+        mDao.deleteAll();
+
+        for (int i = 0; i <= words.length - 1; i++) {
+            Word word = new Word(words[i]);
+            mDao.insert(word);
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -0,0 +1,45 @@
+package com.example.roomwordssample;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.*;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.roomwordssample.dao.WordDao;
+import com.example.roomwordssample.entities.Word;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+    private static WordRoomDatabase INSTANCE;
+
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating
+                            // if no Migration object.
+                            // Migration is not part of this practical.
+                            .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+}
+
